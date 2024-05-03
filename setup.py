0e9bff6c8dc65e377d68a6a26ee40719ee1a2da3
sergiomarco25 <55990938+sergiomarco25@users.noreply.github.com>
2024-05-03 11:42:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup()
+setup(
+name="my_module",
+    version="1.2.0",
+    description="Package to perform xenium benchkmarking",
+    author="Sergio Marco Salas",
+    author_email="sergiomarco.salas@scilifelab.se",
+    packages=["xb","Banksy_py"],
+    install_requires=[
+        # List your module's dependencies here
+    ])
