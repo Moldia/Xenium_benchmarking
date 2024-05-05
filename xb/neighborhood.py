275f3f48be2d875352ed779871d99791f032079f
sergiomarco25 <55990938+sergiomarco25@users.noreply.github.com>
2024-05-05 18:26:15 +0200

    Update neighborhood.py

--- xb/neighborhood.py
+++ xb/neighborhood.py
@@ -14,7 +14,24 @@ import json
 import squidpy as sq
 
 
-def nhood_squidpy(adata,sample_key='sample',radius=50.0,cluster_key='leiden',save=True,plot_path='./',cmap='inferno',vmax=None,vmin=None):
+def nhood_squidpy(adata,sample_key='sample',radius=50,cluster_key='leiden',save=True,plot_path='./',cmap='inferno',vmax=None,vmin=None):
+   """ Compute neighborhood enrichment based on Squidpy's function
+   
+    Parameters:
+    adata (AnnData): AnnData object with the cells of the experiment. 
+    sample_key (str): name of the column where the sample each cell belongs to is specify. It should be a column present in adata.obs
+    radius (int): radius to consider when compuing the spatial neighbors, specified in the scale that adata.obsm['spatial'] is in (typically um)
+    cluster_key (str): name of the column where the cell type of each cell is specified. The neighborhood enrichment will be computed based on this groups 
+    save (Boolean): specify whether the resulting plot should be saved in the paths specified in 'plot_path' or not
+    cmap (str): name of the colormap used to plot the neighborhood enrichment plot
+    vmax (int): maximum value to show in the neighborhood enrcihment plot 
+    vmin (int): minimum value to show in the neighborhood enrichment plot
+
+    Returns:
+    adata1: AnnData object with the neighborhood enrichment scores computed
+
+   """
+    
     anndata_list=[]
     plt.style.use('default')
     for sample in adata.obs[sample_key].unique():
